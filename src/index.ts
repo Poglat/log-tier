bf20875549242b89bad0abc605ceb5cb0bc2c72d
Poglat <poglatgit@gmail.com>
2026-04-09 23:55:33 +1000

    feat: first elements displayed against each other

--- src/index.ts
+++ src/index.ts
@@ -7,31 +7,38 @@ const fileInput = document.getElementById("fileInput")! as HTMLInputElement
 const inputSection = document.getElementById("imageSelection")!
 const rankSection = document.getElementById("rankImages")!
 const tierContainer = document.getElementById("tierContainer")!
-
+const challenger = document.getElementById("challenger")! as HTMLImageElement
+const incumbent = document.getElementById("incumbent")! as HTMLImageElement
 
 const read_files: string[] = []
 
 fileInput.addEventListener('change', startTier)
 
 function startTier() {
+    if (!fileInput.files || fileInput.files.length == 1) {
+        alert("You must give atleast 2 images to sort")
+        return
+    }
+
     inputSection.hidden = true
     rankSection.hidden = false
 
-    if (fileInput.files) {
-        console.log(fileInput.files.length)
-        for (let x = 0; x < fileInput.files.length; x++) {
-            let url = URL.createObjectURL(fileInput.files[x])
-            read_files.push(url)
-
-            createImg(x)
-        }
+    console.log(fileInput.files.length)
+    for (let x = 0; x < fileInput.files.length; x++) {
+        let url = URL.createObjectURL(fileInput.files[x])
+        read_files.push(url)
     }
+    // For visual consistency, start the list with the 2nd file
+    // in list a,b,c this displays [a] vs [b]
+    challenger.src = read_files.shift()!
+    incumbent.src = read_files[0]
+    tierContainer.appendChild(createImg(read_files.shift()!))
 }
 
 
-function createImg(elementIndex: number) {
+function createImg(src: string): HTMLImageElement {
     let img = document.createElement("img")
     img.loading = "lazy"
-    img.src = read_files[elementIndex];
-    tierContainer.appendChild(img)
+    img.src = src;
+    return img
 }
